12a3ff6443f82b84229bdafe7114313c0f3664f5
aleksgolovnya <Rt564oPt28d>
2018-06-25 12:07:08 +0300

    Added template for project

---
+++ src/main/java/com/aleksgolovnya/deansoffice/DeansofficeApplication.java
@@ -0,0 +1,12 @@
+package com.aleksgolovnya.deansoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeansofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeansofficeApplication.class, args);
+    }
+}
