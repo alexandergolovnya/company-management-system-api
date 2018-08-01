1443c6244255bfc5e61a394d4a1abad1f937ab59
aleksgolovnya <Rt564oPt28d>
2018-08-01 16:02:32 +0300

    Added dump data

---
+++ src/main/resources/damp/university_spa_public_students_group.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.students_group (id, group_name, specialty_id) VALUES (1, 'ИВТ/б-11о', 3);
+INSERT INTO public.students_group (id, group_name, specialty_id) VALUES (2, 'ИВТ/б-21о', 3);
+INSERT INTO public.students_group (id, group_name, specialty_id) VALUES (3, 'ИВТ/б-31о', 3);
+INSERT INTO public.students_group (id, group_name, specialty_id) VALUES (4, 'ИВТ/б-41о', 3);
