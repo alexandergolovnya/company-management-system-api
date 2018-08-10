f7708c338aedeae8959758dfdbd542737ad52ed1
aleksgolovnya <aleksandergolovnya@gmail.com>
2018-08-10 16:38:34 +0300

    Add token authentication provider

--- src/main/java/com/universityspa/security/filter/TokenAuthFilter.java
+++ src/main/java/com/universityspa/security/filter/TokenAuthFilter.java
@@ -17,7 +17,8 @@ public class TokenAuthFilter implements Filter {
     /**
      * Method receives token parametr from the request url,
      * if it is null - set isAuthenticated = false,
-     * if token is not null sends it to token provider
+     * if token is not null sends it creates tokenAuthentication objects
+     * and sends it to security context holder, which delegates it to token authentication provider
      * @param servletRequest
      * @param servletResponse
      * @param filterChain
---
+++ src/main/java/com/universityspa/security/provider/TokenAuthProvider.java
@@ -0,0 +1,52 @@
+package com.universityspa.security.provider;
+
+import com.universityspa.entity.auth.Token;
+import com.universityspa.repository.auth.TokenRepository;
+import com.universityspa.security.token.TokenAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Optional;
+
+public class TokenAuthProvider implements AuthenticationProvider {
+
+    @Autowired
+    private TokenRepository tokenRepository;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    /**
+     * Method processes token and connect user to this token
+     * @param authentication
+     * @return tokenAuthentication
+     * @throws AuthenticationException
+     */
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        TokenAuthentication tokenAuthentication = (TokenAuthentication) authentication;
+
+        Optional<Token> token = tokenRepository.findOneByValue(tokenAuthentication.getName());
+
+        if (token.isPresent()) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(token.get().getUser().getEmail());
+            tokenAuthentication.setUserDetails(userDetails);
+            tokenAuthentication.setAuthenticated(true);
+            return tokenAuthentication;
+        } else throw new IllegalArgumentException("Bad token");
+    }
+
+    /**
+     * Supports method for Token Authentication class
+     * @param authentication
+     * @return
+     */
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return TokenAuthentication.class.equals(authentication);
+    }
+}
