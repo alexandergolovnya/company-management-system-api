81776ee3c850174dfd02ce37d98b2a4b5e622b27
aleksgolovnya <aleksandergolovnya@gmail.com>
2018-08-10 03:17:02 +0300

    Create classes login form and user form for receiving data from sign in and sign up forms

---
+++ src/main/java/com/aleksgolovnya/deansoffice/forms/UserForm.java
@@ -0,0 +1,32 @@
+package com.aleksgolovnya.deansoffice.forms;
+
+import lombok.Data;
+
+/**
+ * Class for receiving data from the sign up form
+ * in order to create new user
+ */
+
+@Data
+public class UserForm {
+
+    /**
+     * E-mail of the user
+     */
+    private String email;
+
+    /**
+     * Password of the user
+     */
+    private String password;
+
+    /**
+     * Id of the student to which belongs this user
+     */
+    private Long studentId;
+
+    /**
+     * Id of the teacher to which belongs this user
+     */
+    private Long teacherId;
+}
--- src/main/java/com/aleksgolovnya/deansoffice/service/people/StudentService.java
+++ src/main/java/com/aleksgolovnya/deansoffice/service/people/StudentService.java
@@ -6,8 +6,6 @@ import com.aleksgolovnya.deansoffice.service.CommonCrudService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface StudentService extends CommonCrudService<Student, StudentDto> {
 
     Student addStudent(StudentDto studentDto);
