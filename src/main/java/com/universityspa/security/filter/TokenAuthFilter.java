644c690d11eff72e4087ecb15139f20cc0bc6bc8
aleksgolovnya <aleksandergolovnya@gmail.com>
2018-08-10 16:22:45 +0300

    Add token authentication filter

---
+++ src/main/java/com/universityspa/security/filter/TokenAuthFilter.java
@@ -0,0 +1,46 @@
+package com.universityspa.security.filter;
+
+import com.universityspa.security.token.TokenAuthentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class TokenAuthFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    /**
+     * Method receives token parametr from the request url,
+     * if it is null - set isAuthenticated = false,
+     * if token is not null sends it to token provider
+     * @param servletRequest
+     * @param servletResponse
+     * @param filterChain
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        String token = request.getParameter("token");
+
+        TokenAuthentication tokenAuthentication = new TokenAuthentication(token);
+        if (token == null) {
+            tokenAuthentication.setAuthenticated(false);
+        } else {
+            SecurityContextHolder.getContext().setAuthentication(tokenAuthentication);
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
