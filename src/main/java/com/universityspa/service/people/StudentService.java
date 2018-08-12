7b8c326c06e262c8a95fc82cc97215eb23fd1e7e
aleksgolovnya <aleksandergolovnya@gmail.com>
2018-08-12 17:11:50 +0300

    Improve methods of student service, add exception handling

--- src/main/java/com/universityspa/service/people/StudentService.java
+++ src/main/java/com/universityspa/service/people/StudentService.java
@@ -2,20 +2,16 @@ package com.universityspa.service.people;
 
 import com.universityspa.dto.StudentDto;
 import com.universityspa.entity.Student;
+import com.universityspa.exception.NotFoundException;
 import com.universityspa.service.CommonCrudService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface StudentService extends CommonCrudService<Student, StudentDto> {
 
-    Student addStudent(StudentDto studentDto);
-    void deleteStudent(Long id);
-    Student editStudent(StudentDto studentDto);
-    Page<Student> getAll(Pageable pageable);
-    Student getById(Long id);
-
-    @Override
-    default StudentDto covertToDto(Student student) {
-        throw new RuntimeException("Not implemented");
-    }
+    StudentDto addStudent(StudentDto studentDto);
+    void deleteStudent(Long id) throws NotFoundException;
+    StudentDto editStudent(Long id, StudentDto studentDto) throws NotFoundException;
+    Page<StudentDto> getAll(Pageable pageable);
+    StudentDto getById(Long id) throws NotFoundException;
 }
