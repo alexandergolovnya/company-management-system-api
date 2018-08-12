39ce505013772950713f71c5336cb80d8a4ee9fc
aleksgolovnya <aleksandergolovnya@gmail.com>
2018-08-13 01:42:13 +0300

    Add security roles filters to the department service

--- src/main/java/com/universityspa/service/university/DepartmentService.java
+++ src/main/java/com/universityspa/service/university/DepartmentService.java
@@ -6,13 +6,25 @@ import com.universityspa.exception.NotFoundException;
 import com.universityspa.service.abstracts.CommonCrudService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface DepartmentService extends CommonCrudService<Department, DepartmentDto> {
 
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     DepartmentDto addDepartment(DepartmentDto department);
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     void deleteDepartment(Long id) throws NotFoundException;
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     DepartmentDto editDepartment(Long id, DepartmentDto department) throws NotFoundException;
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     Page<DepartmentDto> getAll(Pageable pageable);
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     DepartmentDto getById(Long id) throws NotFoundException;
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     Page<DepartmentDto> getFacultyDepartments(Long id, Pageable pageable);
 }
