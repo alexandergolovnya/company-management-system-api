fa4a44b494b6606141a1c0c2c535fd688cde6192
aleksgolovnya <aleksandergolovnya@gmail.com>
2018-08-12 17:47:55 +0300

    Improve schedule, student, student group, subject and teacher repositories

--- src/main/java/com/universityspa/repository/ScheduleRepository.java
+++ src/main/java/com/universityspa/repository/ScheduleRepository.java
@@ -8,22 +8,36 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
+/**
+ * Implementation of JpaRepository for Schedule entity
+ */
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-    Schedule getById(Long id);
-
-//    @Query("SELECT count() from schedule " +
-//            "inner join teacher t on teacher_id = t.id " +
-//            "where teacher_id = :id")
-//    Optional<Schedule> getTeacherWorkLoad(@Param("id") Long id);
-
+    /**
+     * Select the number of teacher lessons from schedule
+     *
+     * @param id of the teacher
+     * @return number of teacher lessons
+     */
     @Query("SELECT COUNT(s) FROM Schedule s WHERE s.teacherId=:id")
     Long getTeacherWorkLoad(@Param("id") Long id);
 
+    /**
+     * Select all lessons for teacher from schedule
+     *
+     * @param id of the teacher
+     * @return List<Schedule>
+     */
     @Query("SELECT s FROM Schedule s WHERE s.teacherId=:id")
     List<Schedule> getTeacherLessons(@Param("id") Long id);
 
-    /** Получить всех преподавателей данной кафедры */
+    /**
+     * Get journal for the lesson in the schedule
+     *
+     * @param id of the journal
+     * @return List<Journal>
+     */
     @Query("SELECT j FROM Journal j WHERE j.scheduleId=:id")
     List<Journal> getJournalForScheduleRecord(@Param("id") Long id);
 }
