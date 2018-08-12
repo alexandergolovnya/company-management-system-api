3cc37e5d7f8221a5aa1e862bf2c2fe0f4b48a9a3
aleksgolovnya <aleksandergolovnya@gmail.com>
2018-08-13 01:01:22 +0300

    Add security roles filter to faculty service

--- src/main/java/com/universityspa/service/university/FacultyService.java
+++ src/main/java/com/universityspa/service/university/FacultyService.java
@@ -6,12 +6,22 @@ import com.universityspa.exception.NotFoundException;
 import com.universityspa.service.abstracts.CommonCrudService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface FacultyService extends CommonCrudService<Faculty, FacultyDto> {
 
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     FacultyDto addFaculty(FacultyDto facultyDto);
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     void deleteFaculty(Long id) throws NotFoundException;
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     FacultyDto editFaculty(Long id, FacultyDto facultyDto) throws NotFoundException;
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     Page<FacultyDto> getAll(Pageable pageable);
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     FacultyDto getById(Long id) throws NotFoundException;
 }
