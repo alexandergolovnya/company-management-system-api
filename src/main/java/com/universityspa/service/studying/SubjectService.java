5fd1489f0776cf0938dee6caadc061ceba2fd401
aleksgolovnya <aleksandergolovnya@gmail.com>
2018-08-12 18:20:39 +0300

    Add security role filters to subject service

--- src/main/java/com/universityspa/service/studying/SubjectService.java
+++ src/main/java/com/universityspa/service/studying/SubjectService.java
@@ -6,12 +6,22 @@ import com.universityspa.exception.NotFoundException;
 import com.universityspa.service.CommonCrudService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface SubjectService extends CommonCrudService<Subject, SubjectDto> {
 
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     SubjectDto addSubject(SubjectDto subjectDto);
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     void deleteSubject(Long id) throws NotFoundException;
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     SubjectDto editSubject(Long id, SubjectDto subjectDto) throws NotFoundException;
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     Page<SubjectDto> getAll(Pageable pageable);
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     SubjectDto getById(Long id) throws NotFoundException;
 }
