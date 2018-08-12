2691a74e712bfb6557f6707265f99b600d34f003
aleksgolovnya <aleksandergolovnya@gmail.com>
2018-08-12 17:10:34 +0300

    Add security roles filters to the methods of user and teacher services

--- src/main/java/com/universityspa/service/auth/UserService.java
+++ src/main/java/com/universityspa/service/auth/UserService.java
@@ -7,16 +7,32 @@ import com.universityspa.exception.NotUniqueCredentialsException;
 import com.universityspa.forms.UserForm;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface UserService {
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     UserDto createStudentUser(UserForm userForm) throws NotUniqueCredentialsException, EmptyRequestDataException;
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     UserDto createTeacherUser(UserForm userForm) throws NotUniqueCredentialsException, EmptyRequestDataException;
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     UserDto createAdminUser(UserForm userForm) throws NotUniqueCredentialsException, EmptyRequestDataException;
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     UserDto editUser(Long id, UserForm userForm) throws NotFoundException;
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     void deleteUserFromDatabase(Long id) throws NotFoundException;
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     void setDeletedState(Long id) throws NotFoundException;
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     Page<UserDto> getAll(Pageable pageable);
+
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'TEACHER')")
     UserDto getById(Long id) throws NotFoundException;
 
 }
--- src/main/java/com/universityspa/service/people/TeacherService.java
+++ src/main/java/com/universityspa/service/people/TeacherService.java
@@ -4,20 +4,32 @@ import com.universityspa.dto.TeacherDto;
 import com.universityspa.entity.Schedule;
 import com.universityspa.entity.Teacher;
 import com.universityspa.service.CommonCrudService;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 public interface TeacherService extends CommonCrudService<Teacher, TeacherDto> {
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     Teacher addTeacher(TeacherDto teacher);
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     void deleteTeacher(Long id);
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'ADMIN')")
     Teacher editTeacher(TeacherDto teacher);
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     List<Teacher> getAll();
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     Teacher getById(Long id);
-//    List<Subject> getTeacherSubjects(Long id);
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'ADMIN')")
     List<Schedule> getTeachersSchedule(Long id);
 
     @Override
-    default TeacherDto covertToDto(Teacher entity) {
+    default TeacherDto convertToDto(Teacher entity) {
         throw new RuntimeException("Not implemented");
     }
 }
