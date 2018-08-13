7eceaf839a6a0b68bc293b0e36b8557e68cf7b37
aleksgolovnya <aleksandergolovnya@gmail.com>
2018-08-13 11:54:07 +0300

    Add security roles filter to journal service

--- src/main/java/com/universityspa/service/studying/JournalService.java
+++ src/main/java/com/universityspa/service/studying/JournalService.java
@@ -6,20 +6,40 @@ import com.universityspa.exception.NotFoundException;
 import com.universityspa.service.abstracts.CommonCrudService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 public interface JournalService extends CommonCrudService<Journal, JournalDto> {
 
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'ADMIN')")
     JournalDto addJournal(JournalDto journalDto);
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'ADMIN')")
     void deleteJournal(Long id) throws NotFoundException;
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'ADMIN')")
     JournalDto editJournal(Long id, JournalDto journal) throws NotFoundException;
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     Page<JournalDto> getAll(Pageable pageable);
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     JournalDto getById(Long id) throws NotFoundException;
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     Page<JournalDto> getJournalRecordsForStudent(Long id, Pageable pageable);
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     Page<JournalDto> getStudentPasses(Long id, Pageable pageable);
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     Long getStudentPassesCount(Long id);
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     Page<JournalDto> getStudentMarks(Long id, Pageable pageable);
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     Page<JournalDto> getJournalForScheduleRecord(Long id, Pageable pageable);
 
 }
