4a41e95084d8bc3833bf9ec92aec367d10946d64
aleksgolovnya <aleksandergolovnya@gmail.com>
2018-08-13 12:22:30 +0300

    Add security roles filter for student group service

--- src/main/java/com/universityspa/service/people/StudentsGroupService.java
+++ src/main/java/com/universityspa/service/people/StudentsGroupService.java
@@ -6,13 +6,25 @@ import com.universityspa.exception.NotFoundException;
 import com.universityspa.service.abstracts.CommonCrudService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface StudentsGroupService extends CommonCrudService<StudentsGroup, StudentsGroupDto> {
 
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     StudentsGroupDto addStudentsGroup(StudentsGroupDto studentsGroupDto);
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     void deleteStudentsGroup(Long id) throws NotFoundException;
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     StudentsGroupDto editStudentsGroup(Long id, StudentsGroupDto studentsGroupDto) throws NotFoundException;
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     Page<StudentsGroupDto> getAll(Pageable pageable);
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     StudentsGroupDto getById(Long id) throws NotFoundException;
+
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT', 'ADMIN')")
     Page<StudentsGroupDto> getSpecialtyStudentGroups(Long id, Pageable pageable);
 }
