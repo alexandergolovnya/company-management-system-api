c419e20f0f9d2c59b5ebbc40defb177f32a3059a
Alexander Golovnya <aleksandergolovnya@gmail.com>
2019-09-09 15:13:55 +0300

    Remove deprecated tests and sql-scripts.
    Remove Selenium tests and sql-scripts with initial data.

--- src/main/resources/sql-data-scripts/users.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (id, email, first_name, middle_name, last_name, depa VALUES
-  (1, 'admin', 'Александр', 'Константинович', 'Головня' '$2a$10$IHNNUpR3EIjZOrXON9HW8.Tl.1fQJQQiAwWwNuWthRiZWJB7mBNke', 'ROLE_ADMIN', 'ACTIVE')
